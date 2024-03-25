d205d5134e3a762f2227f29af21e2f310581bb39
unknown <kimyeji546408@gmail.com>
2024-03-25 17:20:00 +0900

    java mySql_db

---
+++ src/main/java/org/example/domain/article/model/Repository.java
@@ -0,0 +1,14 @@
+package org.example.domain.article.model;
+
+import java.util.ArrayList;
+
+public interface Repository {
+    void makeTestData();
+    ArrayList<Article> findArticleByKeyword(String keyword);
+    Article findArticleById(int id);
+    void deleteArticle(Article article);
+    void updateArticle(Article article, String newTitle, String newBody);
+    ArrayList<Article> findAll();
+    Article saveArticle(String title, String body);
+
+}
